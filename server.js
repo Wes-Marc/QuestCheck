ee17f764edc1b1c66901ff874165118794703c35
Wes Marc <weslei047@gmail.com>
2023-10-15 19:45:17 -0300

    Added body style, font and colors. Finished header

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
 app.set("views", "views");
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.render("homepage");
